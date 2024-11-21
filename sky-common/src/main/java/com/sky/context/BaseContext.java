88fc8fbaf5c7314f195237978086413dce308bad
liuyongqin <2841150302@qq.com>
2024-11-21 20:26:56 +0800

    新增员工业务代码,使用ThreadLocal获取id.

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -2,7 +2,8 @@ package com.sky.context;
 
 public class BaseContext {
 
-    public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
+    public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();  // 线程变量,一次请求为一个线程,ThreadLocal为每个线程提供一个变量,
+                                                                        // 一个线程中存储一个值,只能在同一个线程中获取和设置值
 
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
